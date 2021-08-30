2cda966f01247ce2bf7db4ae1d436729402c208f
Zyrhine <contact@jeb.id.au>
2021-08-30 22:26:43 +1000

    Add required components for groups and chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { ChannelListComponent } from './channel-list/channel-list.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { GroupListComponent } from './group-list/group-list.component';
+import { GroupIndexComponent } from './group-index/group-index.component';
+import { GroupSettingsComponent } from './group-settings/group-settings.component';
+import { GroupComponent } from './group/group.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    ChannelListComponent,
+    MemberListComponent,
+    GroupListComponent,
+    GroupIndexComponent,
+    GroupSettingsComponent,
+    GroupComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/group-settings/group-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'group-settings',
+  templateUrl: './group-settings.component.html',
+  styleUrls: ['./group-settings.component.css']
+})
+export class GroupSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
